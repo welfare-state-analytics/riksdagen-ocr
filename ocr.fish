2174eecade139479b27e443d5720bc92f8a7426c
Väinö Yrjänäinen <vainoyrjanainen@icloud.com>
2021-06-08 16:09:45 +0200

    Fix scripts

--- ocr.fish
+++ ocr.fish
@@ -1,6 +1,7 @@
-for filename in images/personregister-*.jpg
+set plainname $argv[1]
+for filename in images/$plainname/*.jpg
     set plain_filename (string replace .jpg "" $filename)
-    set plain_filename (string replace images/ "" $plain_filename)
+    set plain_filename (string replace images/$plainname/ "" $plain_filename)
     echo $filename
     echo $plain_filename
     tesseract -l swe $filename altofiles/$plain_filename alto
--- pipeline.fish
+++ pipeline.fish
@@ -1,5 +1,10 @@
 echo $argv
-rm images/*
-pdfimages -j $argv[1] images/personregister
-fish ocr.fish
-rm images/*
+set firstname $argv[1]
+set plainname (string replace pdfs/ "" $firstname)
+set plainname (string replace .pdf "" $plainname)
+rm -rf images/$plainname
+mkdir images/$plainname
+pdfimages -j $argv[1] images/$plainname/$plainname
+fish ocr.fish $plainname
+
+rm -rf images/$plainname
