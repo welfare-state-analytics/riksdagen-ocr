d4e1b8943395a177552c129fb279171384778ef7
ninpnin <vainoyrjanainen@icloud.com>
2021-11-15 16:55:10 +0100

    Read in statskalender

---
+++ convert.py
@@ -0,0 +1,32 @@
+import pandas as pd
+import os
+import sys
+from pathlib import Path
+import subprocess
+
+years = pd.read_csv("metadata/years.csv")
+print(years)
+
+p = Path(sys.argv[1])
+for _, row in years.iterrows():
+	start = row["start_page"]
+	end = row["end_page"]
+	command = "pdfimages -png "
+	command = command + "-f " + str(start) + " -l " + str(end) + " "
+
+	files = list(p.glob("*" + str(row["year"]) + ".pdf"))
+	
+	if len(files) > 0:
+		plainname = str(files[0]).split("/")[-1].split(".")[0]
+		command = command + str(files[0]) + " "
+		command = command + " " + "images/" + plainname
+		print(command)
+		subprocess.call(command.split())
+
+		p1 = Path("images/")
+
+		for fname in p1.glob("*.png"):
+			plain_fname = str(fname).split(".")[0].split("/")[-1]
+			tess_command = ["tesseract", str(fname), "statscalender/" + plain_fname, "-l", "swe", "alto"]
+			print(" ".join(tess_command))
+			subprocess.call(tess_command)
