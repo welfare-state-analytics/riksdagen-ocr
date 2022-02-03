937f901035b886020fd280652a81ac2985ce45f3
Robin Saberi <robin.saberinasseri@gmail.com>
2022-02-03 17:05:18 +0100

    margin detection prototype

--- margin-detection.py
+++ margin-detection.py
@@ -18,80 +18,85 @@ def api():
 
 # Preprocess image
 original = cv2.imread("riksdagen-images/test0.png")#, cv2.IMREAD_GRAYSCALE)
-img = cv2.cvtColor(original, cv2.COLOR_BGR2GRAY)
-blur = cv2.GaussianBlur(img, (5,5), 0)
-_, otsu = cv2.threshold(img, 0, 255, cv2.THRESH_BINARY+cv2.THRESH_OTSU)
-_, blurotsu = cv2.threshold(blur, 0, 255, cv2.THRESH_BINARY+cv2.THRESH_OTSU)
 
-# Detect margins (mark cells in row-column pairs which both have >0.9 white pixels)
-nrows, ncols = blurotsu.shape
-cols = (blurotsu == 255).sum(axis=0) > nrows * 0.9
-rows = (blurotsu == 255).sum(axis=1) > ncols * 0.9
-blurotsu = np.zeros((nrows, ncols), dtype=np.uint8)
-blurotsu[rows] += 255
-blurotsu[:,cols] += 255
-blurotsu[blurotsu == 255*2] = 255
+def preprocess_image(img):
+	img = cv2.cvtColor(original, cv2.COLOR_BGR2GRAY)
+	img = cv2.GaussianBlur(img, (5,5), 0)
+	_, img = cv2.threshold(img, 0, 255, cv2.THRESH_BINARY+cv2.THRESH_OTSU)
+	return img
+
+def detect_margins(img, thresh_x=0.9, thresh_y=0.9):
+	'''Detect margins (mark cells in row-column pairs which both have >0.9 white pixels)'''
+	rows = (img == 255).sum(axis=1) > img.shape[1] * thresh_x
+	cols = (img == 255).sum(axis=0) > img.shape[0] * thresh_y
+	img = np.zeros(img.shape, dtype=img.dtype)
+	img[rows] += 255
+	img[:,cols] += 255
+	img[img == 255*2] = 255
+	return img
+
+def detect_boxes(img):
+	contours, _ = cv2.findContours(img.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+	return contours[1:]
+
+def merge_boxes(img, boxes, thresh_x=5):
+	rects, rectsUsed, acceptedRects = [], [], []
+
+	# Just initialize bounding rects and set all bools to false
+	for cnt in cnts:
+	    rects.append(cv2.boundingRect(cnt))
+	    rectsUsed.append(False)
+	rects.sort(key = lambda x: x[0])
+
+	# Iterate all initial bounding rects
+	for supIdx, supVal in enumerate(rects):
+	    if (rectsUsed[supIdx] == False):
+
+	        # Initialize current rect
+	        currxMin = supVal[0]
+	        currxMax = supVal[0] + supVal[2]
+	        curryMin = supVal[1]
+	        curryMax = supVal[1] + supVal[3]
+
+	        # This bounding rect is used
+	        rectsUsed[supIdx] = True
+
+	        # Iterate all initial bounding rects
+	        # starting from the next
+	        for subIdx, subVal in enumerate(rects[(supIdx+1):], start = (supIdx+1)):
+
+	            # Initialize merge candidate
+	            candxMin = subVal[0]
+	            candxMax = subVal[0] + subVal[2]
+	            candyMin = subVal[1]
+	            candyMax = subVal[1] + subVal[3]
+
+	            # Check if x distance between current rect
+	            # and merge candidate is small enough
+	            if (candxMin <= currxMax + thresh_x):
+
+	                # Reset coordinates of current rect
+	                currxMax = candxMax
+	                curryMin = min(curryMin, candyMin)
+	                curryMax = max(curryMax, candyMax)
+
+	                # Merge candidate (bounding rect) is used
+	                rectsUsed[subIdx] = True
+	            else:
+	                break
+
+	        # No more merge candidates possible, accept current rect
+	        acceptedRects.append([currxMin, curryMin, currxMax - currxMin, curryMax - curryMin])
+
+	for rect in acceptedRects:
+	    img = cv2.rectangle(img, (rect[0], rect[1]), (rect[0] + rect[2], rect[1] + rect[3]), (121, 11, 189), 2)
+	return img
 
 # Find boxes
-contours, _ = cv2.findContours(blurotsu.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-
-# Merge boxes
-cnts = contours[1:] # First box is whole page
-rects, rectsUsed, acceptedRects = [], [], []
-
-# Just initialize bounding rects and set all bools to false
-for cnt in cnts:
-    rects.append(cv2.boundingRect(cnt))
-    rectsUsed.append(False)
-rects.sort(key = lambda x: x[0])
-
-# Merge threshold for x coordinate distance
-xThr = 5 # does not seem to do anything atm
-
-# Iterate all initial bounding rects
-for supIdx, supVal in enumerate(rects):
-    if (rectsUsed[supIdx] == False):
-
-        # Initialize current rect
-        currxMin = supVal[0]
-        currxMax = supVal[0] + supVal[2]
-        curryMin = supVal[1]
-        curryMax = supVal[1] + supVal[3]
-
-        # This bounding rect is used
-        rectsUsed[supIdx] = True
-
-        # Iterate all initial bounding rects
-        # starting from the next
-        for subIdx, subVal in enumerate(rects[(supIdx+1):], start = (supIdx+1)):
-
-            # Initialize merge candidate
-            candxMin = subVal[0]
-            candxMax = subVal[0] + subVal[2]
-            candyMin = subVal[1]
-            candyMax = subVal[1] + subVal[3]
-
-            # Check if x distance between current rect
-            # and merge candidate is small enough
-            if (candxMin <= currxMax + xThr):
-
-                # Reset coordinates of current rect
-                currxMax = candxMax
-                curryMin = min(curryMin, candyMin)
-                curryMax = max(curryMax, candyMax)
-
-                # Merge candidate (bounding rect) is used
-                rectsUsed[subIdx] = True
-            else:
-                break
-
-        # No more merge candidates possible, accept current rect
-        acceptedRects.append([currxMin, curryMin, currxMax - currxMin, curryMax - curryMin])
-
-for rect in acceptedRects:
-    img = cv2.rectangle(original, (rect[0], rect[1]), (rect[0] + rect[2], rect[1] + rect[3]), (121, 11, 189), 2)
-#
-#cv2.imwrite('riksdagen-images/test-final.png', original)
+img = preprocess_image(original)
+img = detect_margins(img)
+cnts = detect_boxes(img)
+original = merge_boxes(original, cnts)
 cv2.imshow('img', original)
 cv2.waitKey(0)
 
